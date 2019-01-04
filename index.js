48b3350ddb14605d817b3a7849e07669aa214dd2
lakhansamani <lakhan.m.samani@gmail.com>
2019-01-04 12:21:00 +0530

    Add test for base64 conversion

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var pdfParser = new PDFParser(this,1);
 pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
 pdfParser.on("pdfParser_dataReady", pdfData => {
   console.log(pdfParser.getRawTextContent());
+  console.log('binary', Buffer.from(pdfParser.getRawTextContent()).toString('base64'))
 });
 
 pdfParser.loadPDF(pdf_path);
