fce99ac4a06b1257cf759ac424613bae57c0a726
lakhansamani <lakhan.m.samani@gmail.com>
2019-01-04 11:24:49 +0530

    Change sample

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var pdfUtil = require('pdf-to-text');
-var pdf_path = "sample.pdf";
+var pdf_path = "ds-spotfire-platform-overview-final_1546501843015.pdf";
  
 pdfUtil.pdfToText(pdf_path, function(err, data) {
   if (err) throw(err);
